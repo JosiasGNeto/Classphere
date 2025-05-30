60d174d6cb914679c643ce52ae6b5c148ae3723e
Josias <josiasnieto@hotmail.com>
2025-05-31 00:56:41 +0200

    packetmodels

--- Classphere-Server/Initializers/00_packetmodels.js
+++ Classphere-Server/Initializers/00_packetmodels.js
@@ -1,65 +1,76 @@
-var Parser = require('binary-parser').Parser;
-var StringOptions = { length: 99, zeroTerminated: true };
+const Parser = require('binary-parser').Parser;
 
 module.exports = PacketModels = {
 
-    header: new Parser().skip(1)
-        .string("command", StringOptions),
+    header: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true }),
 
-    login: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("username", StringOptions)
-        .string("password", StringOptions),
+    login: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("username", { zeroTerminated: true })
+        .string("password", { zeroTerminated: true }),
 
-    register: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("email", StringOptions)
-        .string("rg", StringOptions)
-        .string("nome", StringOptions)
-        .string("sobrenome1", StringOptions)
-        .string("sobrenome2", StringOptions)
-        .string("nascimento", StringOptions)
+    register: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("email", { zeroTerminated: true })
+        .string("rg", { zeroTerminated: true })
+        .string("nome", { zeroTerminated: true })
+        .string("sobrenome1", { zeroTerminated: true })
+        .string("sobrenome2", { zeroTerminated: true })
+        .string("nascimento", { zeroTerminated: true })
         .uint8("professor"),
 
-    pos: new Parser().skip(1)
-        .string("command", StringOptions)
+    pos: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
         .int32le("target_x")
         .int32le("target_y"),
 
-    get_user_by_rg: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("rg", StringOptions),
+    get_user_by_rg: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("rg", { zeroTerminated: true }),
 
-    update_user: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("rg", StringOptions)
-        .string("nome", StringOptions)
-        .string("sobrenome1", StringOptions)
-        .string("sobrenome2", StringOptions)
-        .string("email", StringOptions)
-        .string("nascimento", StringOptions)
+    update_user: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("rg", { zeroTerminated: true })
+        .string("nome", { zeroTerminated: true })
+        .string("sobrenome1", { zeroTerminated: true })
+        .string("sobrenome2", { zeroTerminated: true })
+        .string("email", { zeroTerminated: true })
+        .string("nascimento", { zeroTerminated: true })
         .uint8("professor"),
 
-    delete_user: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("rg", StringOptions),
-    
-    sit: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("username", StringOptions)
+    delete_user: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("rg", { zeroTerminated: true }),
+
+    sit: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("username", { zeroTerminated: true })
         .uint16le("chair_uid"),
 
-    stand: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("username", StringOptions),
-        
-    chat: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("username", StringOptions)
-        .string("message", StringOptions),
-    
-    door: new Parser().skip(1)
-        .string("command", StringOptions)
-        .string("door_id", StringOptions)
-        .string("action", StringOptions),
+    stand: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("username", { zeroTerminated: true }),
+
+    chat: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("username", { zeroTerminated: true })
+        .string("message", { zeroTerminated: true }),
+
+    door: new Parser()
+        .skip(1)
+        .string("command", { zeroTerminated: true })
+        .string("door_id", { zeroTerminated: true })
+        .string("action", { zeroTerminated: true }),
+
 };
